cc517f371e28ad0993fd9542b3352286d7322013
Joseph Aveltsev <aveltsev@gmail.com>
2019-12-03 18:37:28 +0200

    1.1

--- BinaryHeap/BinaryHeap.cpp
+++ BinaryHeap/BinaryHeap.cpp
@@ -7,11 +7,16 @@ int main()
 
 	min_heap.insert(3);
 	min_heap.insert(2);
+	min_heap.remove(1);
 	min_heap.insert(15);
 	min_heap.insert(5);
 	min_heap.insert(4);
-	min_heap.insert(40);
-
+	min_heap.insert(45);
+	std::cout << min_heap.extract_root() << std::endl;
+	std::cout << min_heap.get_root() << std::endl;
+	min_heap.decrease(2, 1);
+	std::cout << min_heap.get_root() << std::endl;
+	
 	min_heap.print();
 	min_heap.sort();
 	min_heap.print();
